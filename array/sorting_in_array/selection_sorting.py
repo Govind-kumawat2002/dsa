f27277dd5004e1c32d463248fe6632243b5363fc
Govind-kumawat2002 <kumawatritik54@gmail.com>
2024-09-12 14:07:51 +0530

    hello ddsa

--- array/sorting_in_array/selection_sorting.py
+++ array/sorting_in_array/selection_sorting.py
@@ -1,1 +1,20 @@
-def
+def selection_sorting(arr):
+    for x in range(len(arr)-1):
+        # print(x)
+        min_ind = x
+        for j in range(x+1,len(arr)):
+            if arr[min_ind]>arr[j]:
+                min_ind=j
+                # print(min_ind)
+        arr[x], arr[min_ind] = arr[min_ind], arr[x]
+    print ("Sorted array")
+    for x in range(len(arr)):
+        print(arr[x],end=" ") 
+        # i=arr[0]
+        # print(arr[x])
+        
+
+
+
+arr = [2,1,5,9,3,1]
+selection_sorting(arr=arr)
