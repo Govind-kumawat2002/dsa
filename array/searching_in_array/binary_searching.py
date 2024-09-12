bec9fab8b0cb7116ec1d30c919c714e512f711de
Govind-kumawat2002 <kumawatritik54@gmail.com>
2024-09-12 14:30:46 +0530

    binary search in array

---
+++ array/searching_in_array/binary_searching.py
@@ -0,0 +1,17 @@
+def binary_search(arr, search,i ,j ):
+    while i <=j:
+        mid=i+(j-1)//2
+        if arr[mid]==search:
+            return mid
+        
+        elif arr[mid]>search:
+            return binary_search(arr=arr,i=mid+1,j=j,search=search)
+        else:
+            return binary_search(arr=arr,i=i ,j=mid-1,search=search)
+    return -1 # that is edicate the , the element is not present in array 
+
+
+arr = [1,2,3,4,5,6,7,8,89]
+i =0
+j = len(arr)-1
+print(binary_search(arr,search=8,i=i,j=j))
