98aef908e6c38eded15b75c7501ec39d7a50a82f
Govind-kumawat2002 <kumawatritik54@gmail.com>
2024-09-12 16:22:45 +0530

    hello dsa hpow are you

---
+++ array/searching_in_array/2d_array.py
@@ -0,0 +1,20 @@
+
+def searching_2d(matrix, i , j , search):
+    for x in range(len(matrix)):
+        # for y in range(len(x)):
+            # print(y)
+
+
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+    ]
+
+i = 0
+j = len(matrix)-1
+search = 4
+
+searching_2d(matrix, i , j , search)
