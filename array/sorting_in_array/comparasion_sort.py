68836d26c422c5cc2f07b96e8584dd8df1d8cd7e
Govind-kumawat2002 <kumawatritik54@gmail.com>
2024-09-28 11:47:46 +0530

    dsa in python with commaparsion

--- array/sorting_in_array/comparasion_sort.py
+++ array/sorting_in_array/comparasion_sort.py
@@ -1,15 +1,35 @@
+# def sorting(arr):
+#     for i in range(1,len(arr)):
+#         key = arr[i]
+#         j = i-1
+#         while j>=0 and key <arr[j]:
+#             arr[j+1]=arr[j]
+#             j = j-1
+#         arr[j+1]=key
+#     return arr
+
+
+# arr = [4,3,5,1,2]
+
+# result=sorting(arr=arr)
+# print("result ",result)
+
+
 def sorting(arr):
+    # comparision sorting 
+    # i and j 
+    # i == starting point 1 se start  and j is  0 point 
     for i in range(1,len(arr)):
         key = arr[i]
-        j = i-1
-        while j>=0 and key <arr[j]:
+        j = i -1 
+        while j>=0 and key<arr[j]:
             arr[j+1]=arr[j]
-            j = j-1
+            j=j-1
         arr[j+1]=key
-    return arr
+    return  arr
+        
 
+    
 
-arr = [4,3,5,1,2]
-
-result=sorting(arr=arr)
-print("result ",result)
+arr = [1,2,8,5,5]
+sorting(arr)
