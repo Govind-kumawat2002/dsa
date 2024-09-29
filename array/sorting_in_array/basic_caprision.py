4a0a028a3fa00643bcbd7ade0ea8c3ddef139320
Govind-kumawat2002 <kumawatritik54@gmail.com>
2024-09-29 11:13:38 +0530

    dsa with python

--- array/sorting_in_array/basic_caprision.py
+++ array/sorting_in_array/basic_caprision.py
@@ -2,6 +2,10 @@ def com_sorting(arr):
     for i in range(1, len(arr)):
         j = i-1
         key = arr[j]
-        while j<=0 and key < 
+        while j>=0 and key < arr[j]:
+             arr[j+1]=arr[j]
+             j=j-1
+        arr[j+1]=key
+
 arr = [1,2,5,0,8,5]
 com_sorting(arr)
