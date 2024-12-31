4579de885186585130a2dfa45193e3bf303e5b35
govind-kumawat2002 <kumawatritik54@gmail.com>
2024-12-31 11:51:36 +0530

    hello hii

---
+++ strings_question/basic.py
@@ -0,0 +1,49 @@
+# Longest Substring Without Repeating Characters
+# def longest_substring(string):
+#     c = ""
+#     l = []
+#     for i in string:
+#         # c.append(i) 
+#         if c!=i:
+#             l.append(i) 
+#         elif c ==i:
+#             c==i
+#     return c
+# Given 
+
+
+
+        # c =i 
+        # if c!=i:
+        #     l[0].append(i)
+        # elif c==i:
+        #     c=i
+        
+# given a string s, return the longest 
+# palindromic
+ 
+# substring
+#  in s.
+def palindromic(a):
+    c = ""
+    for i in a:
+        c = i
+        # if c!=i:
+    return c
+            
+
+
+
+
+a = "bbca"
+
+result =palindromic(a)
+print(result)
+        
+
+
+
+
+string = "abcabcbc"
+result=longest_substring(string)
+print(result)
