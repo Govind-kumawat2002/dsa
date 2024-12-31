044214801d5c7f306b125bad5d1e070f58f5dfae
govind-kumawat2002 <kumawatritik54@gmail.com>
2024-12-31 17:21:32 +0530

    hello dsa bhai aaja

--- strings_question/basic.py
+++ strings_question/basic.py
@@ -24,26 +24,136 @@
  
 # substring
 #  in s.
-def palindromic(a):
-    c = ""
-    for i in a:
-        c = i
-        # if c!=i:
-    return c
+# def palindromic(a):
+#     c = ""
+#     for i in a:
+#         c+=i
+
+
+        
+#         # if c!=i:
+#         #     # c ==i 
+
+#         #     c += i
+#         # elif c ==i:
+#         #         c+=i
+
+                
+        
+#     return c
             
 
 
 
 
-a = "bbca"
+# a = "caaa"
 
-result =palindromic(a)
-print(result)
+# result =palindromic(a)
+# print(result)
+# def longest_substring_without_repeating(s):
+#     longest = 0
+#     substring = ""
+
+#     for char in s:
+#         if char in substring:
+#             substring = substring[substring.index(char) + 1:]
+#         substring += char
+#         longest = max(longest, len(substring))
+    
+#     return longest,substring
+
+# # Example usage
+# s = "abcabcbb"
+# print(longest_substring_without_repeating(s))
         
 
 
 
 
-string = "abcabcbc"
-result=longest_substring(string)
-print(result)
+# string = "abcabcbc"
+# result=longest_substring(string)
+# print(result)
+
+
+# def longest_substring_without_repeating(s):
+#     longest = 0
+#     start = 0
+
+#     for end in range(len(s)):
+#         for i in range(start, end):
+#             if s[i] == s[end]:
+#                 start = i + 1
+#                 break
+#         longest = max(longest, end - start + 1)
+    
+#     return longest
+
+# # Example usage
+# s = "abcabcbb"
+# print(longest_substring_without_repeating(s))
+
+# Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+# Symbol       Value
+# I             1
+# V             5
+# X             10
+# L             50
+# C             100
+# D             500
+# M             1000
+
+# def roman(dct,target):
+#     for x in target:
+        
+        
+
+
+
+# dct={"I":1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+# target = 'III'
+
+# print(roman(dct,target))
+    
+# def roman_to_int(roman):
+#     roman_values = {
+#         'I': 1, 'V': 5, 'X': 10, 'L': 50,
+#         'C': 100, 'D': 500, 'M': 1000
+#     }
+#     total = 0
+#     prev_value = 0
+
+#     for char in reversed(roman):  # Process from right to left
+#         value = roman_values[char]
+#         if value < prev_value:
+#             total -= value  # Subtract if smaller value precedes a larger value
+#         else:
+#             total += value  # Otherwise, add the value
+#         prev_value = value
+
+#     return total
+
+# # Example usage
+# roman_numeral = "MCMXCIV"  # Example: 1994
+# print(roman_to_int(roman_numeral))  # Output: 1994
+
+
+# Write a function to find the longest common prefix string amongst an array of strings.
+
+# If there is no common prefix, return an empty string "".
+
+def longest_common_function_prefix(lst):
+    y = ''
+    xx=''
+    for x in lst:
+        # print(x)
+        for i in x:
+            if i in y:
+                xx+=y
+
+
+            y+=i
+    return xx 
+            
+lst = ['hello','heo',"hellu"]
+print(longest_common_function_prefix(lst))
