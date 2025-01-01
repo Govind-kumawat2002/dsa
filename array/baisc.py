ebb4cf0946619392dc681b6bfbde35ed905b4a49
govind-kumawat2002 <kumawatritik54@gmail.com>
2025-01-01 19:32:21 +0530

    hellobkdbg

--- array/baisc.py
+++ array/baisc.py
@@ -77,18 +77,112 @@
 
 
 #  Remove Duplicates from Sorted Array
-def dupliacates(arr):
-    for i in range(len(arr)):
-        for j in range(i+1, len(arr)):
-            if arr[i]==arr[j]:
-                del arr[i]
+# def dupliacates(arr):
+#     for i in range(len(arr)):
+#         for j in range(i+1, len(arr)):
+#             if arr[i]==arr[j]:
+#                 del arr[i]
 
-        #    return arr[i]
+#         #    return arr[i]
             
-    return -1 
+#     return -1 
 
 
     
-arr = [1,2,3,4,3]
-result =dupliacates(arr)
-print("the duplicates value in array ",result)
+# arr = [1,2,3,4,3]
+# result =dupliacates(arr)
+# print("the duplicates value in array ",result)
+
+
+
+# remove element form a sorted list 
+# def removes(arr, key ):
+#     for x in arr:
+#         if arr[x] == key:
+#             del arr[x]
+#             return arr
+
+#     return "out of range "
+
+
+# arr = [1,2,3,4,5]
+# key  = 4
+# result =removes(arr,key)
+# print(result)
+
+
+# Next Permutation
+
+# def permulation(arr):
+#    return arr[::-1]
+#     # array = [ x,y,x]
+#     # for i in range(len(arr)):
+#         # for j in range(i+1, len(arr)):
+#         #     for k in range(j+1, len(arr)):
+#                 # # print(arr[i]==arr[j])
+#                 # # arr[k]=arr[i]
+#                 # # arr[j]=arr[k]
+#                 # if arr[i]!=arr[j]:
+#                 #     arr[i]=arr[j]
+#                 # elif arr[j]!=arr[k]:
+#                 #     return arr[j]==arr[k]
+#                 # elif arr[k]!=arr[i]:
+#                 #    return arr[k]==arr[i]
+
+
+
+# arr = [1,2,3] 
+# result =permulation(arr)
+# print(result)
+
+
+# Search in Rotated Sorted Array
+# def searching_in_sorted_array(arr,target ):
+#     for i in range(len(arr)):
+#         if arr[i]==target:
+#             return i
+#     return -1
+    
+
+
+# arr = [1,2,3,5,4]
+# result =searching_in_sorted_array(arr, target = 5)
+# print(result)
+
+
+# Find First and Last Position of Element in Sorted Array
+
+# def find_first(arr ,target ):
+
+#     for i in range(len(arr)):
+        
+#         for j in range(i+1,len(arr)):
+#            if arr[i]==arr[j]:
+#                 if arr[i]==target:
+#                     return [i ,j ]
+#     return [-1,-1]
+                    
+
+    
+# arr = [1,5,5,8,8]
+# result =find_first(arr, target = 8 )
+# print(result)
+
+# Search Insert Position
+
+# def search_insert_position(arr,target):
+#     for x in range(len(arr)):
+#         if arr[x]==target:
+#             return "that is already present in array",x
+        
+
+            
+        
+
+        
+        
+
+    
+# arr = [1,3,4,5]
+# result=search_insert_position(arr,target = 2)
+# print(result)
